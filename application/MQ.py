2832dc5a0ee67655d230b013d5128ab7b2686c16
wtgg <wtlit@qq.com>
2021-02-23 17:10:02 +0800

    debug x1

--- application/MQ.py
+++ application/MQ.py
@@ -1,6 +1,8 @@
 import json
 from datetime import datetime
 
+import pika
+
 from application.ext import RabbitMQ_channel, async
 from config import 国恒信创采集MQ
 
@@ -11,10 +13,13 @@ class Pro:
         self.channel = RabbitMQ_channel(RabbitMQconfig)
 
     def send(self, queue, data):
+        self.channel.queue_declare(queue=queue, durable=True)
         self.channel.basic_publish(
             exchange='',
             routing_key=queue,
-            # body=serialized_value(data),
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # make message persistent
+            ),
             body=json.dumps(data, ensure_ascii=False),
         )
 
@@ -36,8 +41,7 @@ class Cus:
 
     @async
     def run(self):
-        self.channel.queue_declare(queue=self.queue)
-        # self.channel.basic_consume(self.queue, self.callback)
+        self.channel.queue_declare(queue=self.queue, durable=True)
         self.channel.basic_consume(
             queue=self.queue,
             on_message_callback=self.callback,
@@ -45,7 +49,6 @@ class Cus:
         )
         # 开始接收信息，并进入阻塞状态，队列里有信息才会调用callback进行处理
         self.channel.start_consuming()
-
 
 
 if __name__ == '__main__':
